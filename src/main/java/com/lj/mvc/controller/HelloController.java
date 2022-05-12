c35c078830d2c6321ae5d10ce332877baf986285
miku <1348250435@qq.com>
2022-05-12 14:52:46 +0800

    second commit

--- src/main/java/com/lj/mvc/controller/HelloController.java
+++ src/main/java/com/lj/mvc/controller/HelloController.java
@@ -1,17 +1,11 @@
 package com.lj.mvc.controller;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
-//    @RequestMapping(value = "/")
-//    public String index(){
-//        return "index";
-//    }
     @RequestMapping(value = {"/target","/test"},method = {RequestMethod.GET})
     public String toTarget(){
         return "target";
--- src/main/java/com/lj/mvc/controller/ParamController.java
+++ src/main/java/com/lj/mvc/controller/ParamController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ParamController {
